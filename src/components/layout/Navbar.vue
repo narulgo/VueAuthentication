bb20590c90fb4d317d4aced40b8d408c68a497a2
Narul <naruln9494@gmail.com>
2021-04-24 12:37:22 +0600

    first commit

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong></strong>
+            </router-link>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <div v-if="isAuthenticated">
+                            Logged in
+                        </div>
+                        <div v-else>
+                            <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
+                            <router-link to="/log-in" class="button is-light">Log in</router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    name: 'Navbar',
+    computed: {
+        ...mapState(['isAuthenticated'])
+    }
+}
+</script>
